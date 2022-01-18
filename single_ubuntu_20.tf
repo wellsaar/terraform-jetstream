258b4afe2cf8708419cd6709a6cbe1df68f36fe9
Aaron Wells <wellsaar@iu.edu>
2022-01-18 15:48:12 -0500

    set up virtual router and virtual router interface

--- single_ubuntu_20.tf
+++ single_ubuntu_20.tf
@@ -41,9 +41,20 @@ resource "openstack_networking_subnet_v2" "terraform_subnet1" {
   name = "terraform_subnet1"
   network_id  = "${openstack_networking_network_v2.terraform_network.id}"
   cidr  = "192.168.0.0/24"
-
+  ip_version  = 4
 }
-
+# setting up virtual router
+resource "openstack_networking_router_v2" "terraform_router" {
+  name = "terraform_router"
+  admin_state_up  = true
+  # id of public network at IU
+  external_network_id = "4367cd20-722f-4dc2-97e8-90d98c25f12e"
+}
+# setting up virtual router interface
+resource "openstack_networking_router_interface_v2" "terraform_router_interface_1" {
+  router_id = "${openstack_networking_router_v2.terraform_router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.terraform_subnet1.id}"
+}
 
 
 #
