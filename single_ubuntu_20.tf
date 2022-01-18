51204d56bf989a364c333da3a285b284279dd116
Aaron Wells <wellsaar@iu.edu>
2022-01-18 16:08:35 -0500

    setup VM and networking instance

--- single_ubuntu_20.tf
+++ single_ubuntu_20.tf
@@ -27,6 +27,7 @@ resource "openstack_compute_secgroup_v2" "terraform_ssh_ping" {
 #   # you will need to paste your public key string here
 #   public_key = ""
 # }
+
 ################
 #Networking
 ################
@@ -56,12 +57,34 @@ resource "openstack_networking_router_interface_v2" "terraform_router_interface_
   subnet_id = "${openstack_networking_subnet_v2.terraform_subnet1.id}"
 }
 
+################
+#VMs
+################
 
-#
-# # creating Ubuntu20 instance
-# resource "openstack_compute_instance_v2" "Ubuntu20" {
-#   name = "terraform-Ubuntu20"
-#   image_id = "8f27559a-9e63-4fb7-9704-09526793e2d2"
-#
-#
-# }
+# creating Ubuntu20 instance
+resource "openstack_compute_instance_v2" "Ubuntu20" {
+  name = "terraform_Ubuntu20"
+  # ID of JS-API-Featured-Ubuntu20-Latest
+  image_id  = "8f27559a-9e63-4fb7-9704-09526793e2d2"
+  flavor_id   = "m1.medium"
+  # this public key is set above in security section
+  key_pair  = "wellsaar"
+  security_groups   = ["terraform_ssh_ping, default"]
+
+  metadata = {
+    terraform_controlled = "yes"
+  }
+  network {
+    name = "terraform_network"
+  }
+}
+# creating floating ip from the public ip pool
+resource "openstack_networking_floatingip_v2" "terraform_floatip" {
+  pool = "public"
+}
+
+# assigning floating ip from public pool to Ubuntu20 VM
+resource "openstack_compute_floatingip_associate_v2" "terraform_floatip1" {
+  floating_ip = "${openstack_networking_floatingip_v2.terraform_floatip.address}"
+  instance_id = "${openstack_compute_instance_v2.Ubuntu20.id}"
+}
