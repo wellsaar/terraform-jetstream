4cc207c461cdf2bb7952fbad0bc438deac0fc3d6
Aaron Wells <wellsaar@iu.edu>
2022-01-19 11:36:48 -0500

    moved security section to security.tf

--- single_centos8.tf
+++ single_centos8.tf
@@ -1,45 +1,15 @@
-################
-#Security section
-################
-#creating security group
-resource "openstack_compute_secgroup_v2" "terraform_ssh_ping_centos" {
-  name = "terraform_ssh_ping"
-  description = "Security group with SSH and PING open to 0.0.0.0/0"
-
-  #ssh rule
-  rule{
-    ip_protocol = "tcp"
-    from_port  =  "22"
-    to_port    =  "22"
-    cidr       = "0.0.0.0/0"
-  }
-  rule {
-    from_port   = -1
-    to_port     = -1
-    ip_protocol = "icmp"
-    cidr        = "0.0.0.0/0"
-  }
-
-}
-# #public key that will be installed in the .authorized_keys file on the vm
-# resource "openstack_compute_keypair_v2" "keypair" {
-#   name       = "my_public_key"
-#   # you will need to paste your public key string here
-#   public_key = ""
-# }
-
 
 ################
 #VMs
 ################
 
-# creating Ubuntu20 instance
+# creating CentOS 8 instance
 resource "openstack_compute_instance_v2" "CentOS8" {
-  name = "terraform_Ubuntu20"
+  name = "terraform_CentOS8"
   # ID of JS-API-Featured-CentOS8-Latest
   image_id  = "006c8d04-1966-4706-b67c-5ae532d66683"
   flavor_id   = 3
-  # this public key is set above in security section
+  # this public key is set in security.tf
   key_pair  = "wellsaar"
   security_groups   = ["terraform_ssh_ping", "default"]
 
