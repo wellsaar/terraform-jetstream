69b4c48a12f12f1c0f79da6b8d22521470a76388
Aaron Wells <wellsaar@iu.edu>
2022-05-18 13:29:05 -0400

    updated variables.tf file with better instructions

--- variables.tf
+++ variables.tf
@@ -3,7 +3,8 @@ variable "vm_number" {
 }
 
 variable "public_key" {
-  # replace this with the name of your public ssh key you want to include in the vm
+  # replace this with the name of the public ssh key you uploaded to Jetstream 2
+  # https://docs.jetstream-cloud.org/ui/cli/managing-ssh-keys/
   default = "!!! REPLACE ME WITH YOUR PUBLIC KEY NAME"
 }
 
